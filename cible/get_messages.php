cfc7b4d7980bf96fb708a265f11339db05ba08a9
barraux.a <barraux.a@example.com>
2022-04-13 17:30:19 +0200

    regle de conflit

--- cible/get_messages.php
+++ cible/get_messages.php
@@ -3,12 +3,30 @@ $bdd = new SQLite3('../database/message.db', SQLITE3_OPEN_READWRITE);
 $response = $bdd->query('SELECT * FROM content where discussion="'.$title.'"');
 
 while ($line = $response->fetchArray()) {
-    if ($line['user'] == $_SESSION['name']) {
-        echo "<div style='text-align: right;'>".$line['mess']."</div>";
+    if ($only_new){
+        $vu = explode(",",$line['seen']);
+        if (!in_array($_SESSION["name"], $vu)){
+            if ($line['user'] == $_SESSION['name']) {
+                echo "<div style='text-align: right;'>".$line['mess']."</div>";
+            }
+            else {
+                echo "<div style='text-align: left;'>".$line['mess']." </div><small>".$line['user']."</small>";
+            }
+            array_push($vu, $_SESSION['name']);
+            $vu = implode(',', $vu);
+            $append = $bdd->prepare("UPDATE content SET seen='".$vu."' where ID=".$line['ID']);
+            $append->execute();
+        }
     }
     else {
-        echo "<div style='text-align: left;'>".$line['mess']." </div><small>".$line['user']."</small>";
+        if ($line['user'] == $_SESSION['name']) {
+            echo "<div style='text-align: right;'>".$line['mess']."</div>";
+        }
+        else {
+            echo "<div style='text-align: left;'>".$line['mess']." </div><small>".$line['user']."</small>";
+        }
 
     }
+
 }
  ?>
--- cible/sign_up.php
+++ cible/sign_up.php
@@ -11,37 +11,41 @@ if (isset($_POST['name']) && isset($_POST['password']) && isset($_POST['email'])
 	$date_naissance = date_create($_POST['birth_date']);
 	$birth_date = date_format($date_naissance, 'Ymd');
 
-	if($birth_date <= $date_13) {
-		$bdd = new SQLite3('../database/users.db', SQLITE3_OPEN_READWRITE);
+	$error="";
 
-
-		// Je reagarde si l'user exixte déjà
-	    $response = $bdd->query("SELECT * FROM users");
-
-	    while ($line = $response->fetchArray()) {
-	        if ($line["name"] == $_POST['name']) {
-	            print_r(json_encode("{'error': 'username'}"));
-	            exit();
-	        }
-	        elseif ($line["email"] == $_POST['email']){
-	            print_r(json_encode("{'error': 'email'}"));
-	            exit();
-	        }
-	    }
-	    // Si le nom d'utilisateur et l'email n'est pas utilisé on peut créer le compte
-
-	    $append = $bdd->prepare("INSERT INTO users(name, password, email, birth_date) VALUES(:name, :password, :email, :birth_date)");
-
-	    $append->bindValue(':name', $_POST['name']);
-	    $append->bindValue(':password', $_POST['password']);
-	    $append->bindValue(':email', $_POST['email']);
-	    $append->bindValue(':birth_date', $_POST['birth_date']);
-
-	    $append->execute();
+	if(!$birth_date <= $date_13) {
+		$error.=array("error"=>array());
+		var_dump(json_encode($error));
+		// print_r(json_encode("{'error': 'date'}"));
 
 	}
-	else {
-		print_r(json_encode("{'error': 'date'}"));
+	$bdd = new SQLite3('../database/users.db', SQLITE3_OPEN_READWRITE);
+
+
+	// Je reagarde si l'user exixte déjà
+	$response = $bdd->query("SELECT * FROM users");
+
+	while ($line = $response->fetchArray()) {
+		if ($line["name"] == $_POST['name']) {
+			echo 'name';
+			$error.="'error;': 'username',";
+			// print_r(json_encode("{'error': 'username'}"));
+		}
+		elseif ($line["email"] == $_POST['email']){
+			$error.="'error;': 'email',";
+			// print_r(json_encode("{'error': 'email'}"));
+		}
 	}
+	// Si le nom d'utilisateur et l'email n'est pas utilisé on peut créer le compte
+
+	// $append = $bdd->prepare("INSERT INTO users(name, password, email, birth_date) VALUES(:name, :password, :email, :birth_date)");
+	//
+	// $append->bindValue(':name', $_POST['name']);
+	// $append->bindValue(':password', $_POST['password']);
+	// $append->bindValue(':email', $_POST['email']);
+	// $append->bindValue(':birth_date', $_POST['birth_date']);
+	//
+	// $append->execute();
+	var_dump($error);
 }
  ?>
