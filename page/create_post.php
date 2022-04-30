d7db2e3e2a6de058e254bbbc03f7a4d5f8256291
kalyax <kalyax@kalyax-server.home>
2022-04-30 23:28:08 +0200

    oubli

--- page/create_post.php
+++
@@ -1,9 +0,0 @@
-<?php session_start(); ?>
-<form action='../cible/send_post.php' method="post" enctype="multipart/form-data" onsubmit="return false;">
-    <input type="text" name="title" value="title">
-    <input type="hidden" name="user" value=<?php echo $_SESSION['user_ID']; ?>>
-    <input type="file" name="image">
-    <textarea name="content" rows="8" cols="80"></textarea>
-    <input type="hidden" name="date" value="<?php echo date('Y-m-d H:i:s'); ?>">
-    <input class="button is-white" type="submit" name="" value="poster">
-</form>
