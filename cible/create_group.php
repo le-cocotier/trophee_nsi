f79628d28e1f13bf843142e9891db128a7ca9f5c
barraux.a <barraux.a@example.com>
2022-05-03 19:24:51 +0200

    admin dezs groupes

--- cible/create_group.php
+++ cible/create_group.php
@@ -13,10 +13,11 @@ if (isset($_POST['users'])){
 
     $bdd = new SQLite3($_SERVER["DOCUMENT_ROOT"].'/trophee_nsi/database/message.db', SQLITE3_OPEN_READWRITE);
 
-    $append = $bdd->prepare("INSERT INTO discussion(name, users_ID, 'group') VALUES(:name, :users_ID, :group)");
     if (count($user_IDs) >2){
         $group = 'true';
+        $append = $bdd->prepare("INSERT INTO discussion(name, admin, users_ID, 'group') VALUES(:name, :admin, :users_ID, :group)");
         $append->bindValue(':name', $_POST['users'].','.$_SESSION['name']);
+        $append->bindValue(':admin',$_SESSION['user_ID']);
         $append->bindValue(':group', $group);
         $append->bindValue(':users_ID', implode(',', $user_IDs));
         $append->execute();
@@ -25,6 +26,8 @@ if (isset($_POST['users'])){
     }
     else {
         $group = 'false';
+
+        $append = $bdd->prepare("INSERT INTO discussion(name, users_ID, 'group') VALUES(:name, :users_ID, :group)");
         $append->bindValue(':name', $_POST['users']);
         $append->bindValue(':group', $group);
         $append->bindValue(':users_ID', implode(',', $user_IDs));
@@ -32,9 +35,6 @@ if (isset($_POST['users'])){
         $query = "SELECT ID from discussion where name='DM'";
         $response = $bdd->query($query);
     }
-
-
-
 
     header('location: /trophee_nsi/page/index/index.php?content_type=dm&id='.($response->fetchArray())['ID']);
 }
