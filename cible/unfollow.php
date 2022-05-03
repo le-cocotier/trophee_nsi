349d9748aea170abd447f9eb9090ac082948282b
quipiq <saulnierjeanlou@gmail.com>
2022-05-03 17:55:56 +0200

    notif quasi finis juste détails en plus à finir

--- cible/unfollow.php
+++ cible/unfollow.php
@@ -15,4 +15,13 @@ unset($liste_of_friends[array_search($_POST['user_to_unfollow'], $liste_of_frien
 $liste_of_friends = implode(',', $liste_of_friends);
 $response = $bdd->exec("UPDATE users SET subscriptions=".$nb_subscriptions.", friends='".$liste_of_friends."' where id='".$_POST['user']."'");
 print_r(json_encode(["state"=>"unfollowed"]));
+
+$bdd_notif = new SQLite3($_SERVER["DOCUMENT_ROOT"].'/trophee_nsi/database/notifications.db', SQLITE3_OPEN_READWRITE);
+$response_notif = $bdd_notif->query("SELECT * FROM notifications where user_concerning='".$_POST['user']."'");
+while($line_notif = $response_notif->fetchArray()){
+    //if($line_notif['user_ID']==$_POST['user_to_unfollow']){
+    $response_notif = $bdd_notif->query("DELETE FROM notifications where user_ID='".$_POST['user_to_unfollow']."'");
+    //}
+}
+
  ?>
