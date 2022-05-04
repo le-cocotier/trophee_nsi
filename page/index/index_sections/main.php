8ddd93dbf17fe065ebdc7953deac4918ce8b4858
Kalyax <kalyax@hotmail.com>
2022-05-04 14:38:11 +0200

    css dernier push normalement

--- page/index/index_sections/main.php
+++ page/index/index_sections/main.php
@@ -6,24 +6,4 @@
             <p class="text-under">Lambda est un réseau social open-source créé lors du Trophée NSI 2022. C'est un projet réalisé par 4 élèves cherchant à permettre de communiquer facilement avec d'autres personnes. C'est un projet simpliste, mais cool !</p>
         </div>
     </div>
-    <div class="content-main__band">
-        images wsh
-    </div>
-    <div class="content-main__container">
-        <h2 class="content-main__container__head">
-            Un réseau social assez simple, et assez cool.
-        </h2>
-        <div class="content-main__container__items">
-            <h2>Lorem</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas nobis quisquam molestias ipsum sint necessitatibus magni aspernatur laudantium, natus, itaque odit alias ea! Architecto, eligendi! Earum similique sequi dolores sed!</p>
-        </div>
-        <div class="content-main__container__items">
-            <h2>Lorem</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas nobis quisquam molestias ipsum sint necessitatibus magni aspernatur laudantium, natus, itaque odit alias ea! Architecto, eligendi! Earum similique sequi dolores sed!</p>
-        </div>
-        <div class="content-main__container__items">
-            <h2>Lorem</h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas nobis quisquam molestias ipsum sint necessitatibus magni aspernatur laudantium, natus, itaque odit alias ea! Architecto, eligendi! Earum similique sequi dolores sed!</p>
-        </div>
-    </div>
 </div>
