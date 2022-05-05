2bbeb41afd984744b9ac5e7e0ff1cb22083f84ff
barraux.a <barraux.a@example.com>
2022-05-05 08:24:43 +0200

    modif follow

--- cible/follow.php
+++ cible/follow.php
@@ -3,7 +3,7 @@ include $_SERVER["DOCUMENT_ROOT"].'/trophee_nsi/cible/functions.php';
 $bdd = new SQLite3($_SERVER["DOCUMENT_ROOT"].'/trophee_nsi/database/users.db', SQLITE3_OPEN_READWRITE);
 $response = $bdd->query("SELECT public FROM users where id='".$_POST['user_to_follow']."'");
 $public = $response->fetchArray()['public'];
-if($_POST['accept_user']=='true'){
+if(isset($_POST['accept_user']) && $_POST['accept_user']=='true'){
     $bdd_notif = new SQLite3($_SERVER["DOCUMENT_ROOT"].'/trophee_nsi/database/notifications.db', SQLITE3_OPEN_READWRITE);
     $append = $bdd_notif->prepare("INSERT INTO notifications(user_ID, type, user_concerning) VALUES(:user_ID, :type, :user_concerning)");
     $append->bindValue(':user_ID', $_POST['user_to_follow']);
