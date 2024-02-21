5e2c71e5982a0893657dbcd5740b879e5cbe6f12
Eclynos <Eclynos@outlook.com>
2024-02-21 14:40:52 +0100

    Test fonctionnement scss

--- includes/post.php
+++ includes/post.php
@@ -1,7 +1,7 @@
 <div class="card is-post">
     <div class="post-header">
         <?php if (isset($id)){
-            echo '<button onclick="delete_post('.$id.')">...</button>';
+            echo '<button id="delete" class="button is-delete" onclick="delete_post('.$id.')"><img src="/img/bin.jpg" alt="Delete"> </button>';
         } ?>
         <a class="post-header__user" href="/page/index?content_type=user&id=<?php echo $user_ID; ?>">
             <img class="post-header__user__img" height="32" width="32" src=<?php echo get_pp_src($user_ID); ?> alt="user_photo">
